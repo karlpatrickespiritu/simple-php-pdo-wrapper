5490864a200f8a503a09ac865c2729f76beede86
karlpatrickespiritu <karl.espiritu@corp.mobile.co>
2015-05-28 18:21:46 +0800

    libraries.

--- sample-app/views/partials/header.php
+++ sample-app/views/partials/header.php
@@ -4,10 +4,10 @@
     <title>Simple PHP/PDO Class</title>
 	
 	<!-- CDN -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">-->
 	
 	<!-- ON SERVER -->
-	<!-- <link rel="stylesheet" href="<?php echo LIB_PATH; ?>/bootstrap3.1.1/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="<?php echo LIB_PATH; ?>/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo LIB_PATH; ?>/jbox/Source/jBox.css" />
 
 </head>
